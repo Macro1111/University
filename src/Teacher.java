312d44f16c09405219f6aaded7d784ac52eae4a0
Marco Antonio <marcomolina12@hotmail.com>
2025-05-14 22:22:31 -0500

    Implementation in the teacher class and creation of the SalaryCalculator interface

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,23 @@
-public class Teacher {
+public abstract class Teacher {
+    protected String name;
+    protected double baseSalary;
+    protected SalaryCalculator salaryCalculator;
+
+    public Teacher(String name, double baseSalary, SalaryCalculator salaryCalculator) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+        this.salaryCalculator = salaryCalculator;
+    }
+
+    public double getSalary() {
+        return salaryCalculator.calculateSalary(this);
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
